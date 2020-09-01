130a81a3066a3cfc239558531da98173cecb98fc
Jamie Stewart <stwjme@gmail.com>
2020-08-31 23:27:01 -0400

    loops

---
+++ loops020.js
@@ -0,0 +1,17 @@
+function insertionSort (arr) {
+
+  for (let i = 1; i < arr.length; i++) {
+    let currentVal = arr[i];
+    for (let j = i -1; j >= 0 && arr[j] > currentVal; j --) {
+      arr[j+1] = arr[j]
+      // console.log(arr);
+    }
+    // arr[j + 1] = currentVal;
+    arr = currentVal;
+    console.log("*************");
+    console.log(arr);
+
+  }
+  return arr;
+}
+insertionSort([2, 10, 7, 12, 4, 8])
